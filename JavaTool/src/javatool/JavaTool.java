27c674c5bbe6795e95d2a23aa2ac37f72d425d74
GuillermoDAMC <47129671+GuillermoDAMC@users.noreply.github.com>
2019-09-27 00:58:02 -0500

    Changed wording

--- JavaTool/src/javatool/JavaTool.java
+++ JavaTool/src/javatool/JavaTool.java
@@ -15,7 +15,7 @@ public class JavaTool {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Foo!");
+        System.out.println("Hola mundo!");
     }
     
 }
